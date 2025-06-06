002f7b42fbf6d8999d6c1c2992d2cdcda5dde433
felipe.chagas <felipe.chagas@sga.pucminas.br>
2025-06-06 14:54:09 +0000

    att 6/6

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 function main() {
   console.log("Hello, World!");
 }
-
+function oneoff(){
+    document.getElementById("demo").style.display = "none";
+    
+}
 main();
+
+document.addEventListener('DOMContentLoaded', function() {
+    const myImage = document.getElementById('myImage');
+
+    if (myImage) {
+        myImage.addEventListener('click', function() {
+            if (myImage.src.match('pic_bulbon.gif')) {
+                myImage.src = 'pic_bulboff.gif';
+            } else {
+                myImage.src = 'pic_bulbon.gif';
+            }
+        });
+    }
+});
